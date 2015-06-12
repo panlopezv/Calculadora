43a4260756416694db343222c4bc41168b764915
panlopezv <panlopezv@outlook.com>
2015-06-12 11:56:51 -0600

    Se agrego el menu de calculadora en Main

--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -16,6 +16,13 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int opcion = 0;
+        System.out.println("++--**// Calculadora \\**--++");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicacion");
+        System.out.println("4. Division");
+        System.out.print("Seleccione una operacion: ");
     }
     
 }
