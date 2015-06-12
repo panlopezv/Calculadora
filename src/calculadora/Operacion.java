1cdc858efb964e448da5151e7fb955cd70707515
panlopezv <panlopezv@outlook.com>
2015-06-12 11:19:26 -0600

    Se creo la interfaz Operacion

---
+++ src/calculadora/Operacion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Pablo López
+ */
+public interface Operacion {
+    public int operar(int operador1, int operador2);
+}
