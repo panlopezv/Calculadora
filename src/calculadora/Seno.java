3d7c67e25d568906f6445c9106ee411db44098a3
Cristian <Cristian@Equipo_Flores>
2015-06-15 20:47:50 -0600

    Creacion de la clase Trigonometria y de la clase seno la cual generaliza de trigonometria

---
+++ src/calculadora/Seno.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Seno extends Trigonometrica implements Operacion{
+    
+    public Seno(int operador1) {
+        this.operador1=operador1;
+    }
+    
+    public int multiplicar(){        
+        return (int) Math.sin(operador1);
+    }
+
+    @Override
+    public int operar() {
+        return multiplicar();
+    }
+}
