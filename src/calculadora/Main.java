4ec5d0a9750e9b06c96b3b34f0b97d2c409d92e1
panlopezv <panlopezv@outlook.com>
2015-06-15 19:20:03 -0600

    Correccion del menu de operaciones

--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -17,23 +17,19 @@ import java.util.logging.Logger;
  */
 public class Main {
 
+    static int operador1 = 0;
+    static int operador2 = 0;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         int opcion = 0;
-        int operador1 = 0;
-        int operador2 = 0;
         Operacion operacion;
         Calculadora calculadora;
         BufferedReader lector=new BufferedReader(new InputStreamReader(System.in));
         try {
             System.out.println("++--**// Calculadora \\\\**--++");
-            System.out.println("Ingrese el primer Numero: ");
-            operador1 = Integer.parseInt(lector.readLine());
-            System.out.println("Ingrese el Segundo Numero: ");
-            operador2 = Integer.parseInt(lector.readLine());;
             System.out.println("1. Suma");
             System.out.println("2. Resta");
             System.out.println("3. Multiplicacion");
@@ -42,9 +38,9 @@ public class Main {
 
             opcion = Integer.parseInt(lector.readLine());
             
-            switch (opcion) {
-                
+            switch (opcion) {                
                 case 1:
+                    ingresarAritmetica(lector);
                     operacion = new Suma(operador1,operador2);
                     calculadora= new Calculadora(operacion);
                     calculadora.ejecutarOperacion();
@@ -52,31 +48,50 @@ public class Main {
                     break;
                 
                 case 2:
+                    ingresarAritmetica(lector);
                     operacion = new Resta(operador1,operador2);
                     calculadora= new Calculadora(operacion);
                     calculadora.ejecutarOperacion();
                     System.out.println("El resultado de operar "+operador1+" - "+operador2+" = "+ calculadora.getResultado());
-                    break;
-                    
-                
-                    
-                    
-                    
+                    break;           
                 case 3:
+                    ingresarAritmetica(lector);
                     operacion = new Multiplicacion(operador1,operador2);
                     calculadora= new Calculadora(operacion);
                     calculadora.ejecutarOperacion();
                     System.out.println("El resultado de operar "+operador1+" * "+operador2+" = "+ calculadora.getResultado());
                     break;
                 case 4:
+                    ingresarAritmetica(lector);
                     operacion = new Division(operador1,operador2);
                     calculadora= new Calculadora(operacion);
                     calculadora.ejecutarOperacion();
                     System.out.println("El resultado de operar "+operador1+" / "+operador2+" = "+ calculadora.getResultado());
                     break;
+                default:
+                    break;
             }
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void ingresarAritmetica(BufferedReader lector){
+        try {
+            System.out.println("Ingrese el primer Numero: ");
+            operador1 = Integer.parseInt(lector.readLine());
+            System.out.println("Ingrese el Segundo Numero: ");
+            operador2 = Integer.parseInt(lector.readLine());;
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+    public static void ingresarTrigonometrica(BufferedReader lector){
+        try {
+            System.out.println("Ingrese el Numero: ");
+            operador1 = Integer.parseInt(lector.readLine());
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
         }
     }
     
