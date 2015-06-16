fa2c6ff233d44bd8e5715a1120a8d2b918e20633
Cristian <Cristian@Equipo_Flores>
2015-06-15 21:12:38 -0600

    Creacion de dos clases que se generalizan de Trigonometrica *	Coseno * 	Tangente con los metodos para operar

---
+++ src/calculadora/Tangente.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Tangente extends Trigonometrica implements Operacion{
+    
+    public Tangente(int operador1) {
+        this.operador1=operador1;
+    }
+    
+    public int operacionTangente(){        
+        return (int) Math.sin(operador1);
+    }
+
+    @Override
+    public int operar() {
+        return operacionTangente();
+    }
+    
+}
