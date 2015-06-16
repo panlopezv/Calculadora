57ec5db908e47130c6e2b2f591f9f3bf3564c64a
Cristian <Cristian@Equipo_Flores>
2015-06-15 21:29:00 -0600

    se agrego la clase coseno la cual hacia falta

---
+++ src/calculadora/Coseno.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Coseno extends Trigonometrica implements Operacion{
+    
+    public Coseno(int operador1) {
+        this.operador1=operador1;
+    }
+    
+    public int operacionCoseno(){        
+        return (int) Math.cos(operador1);
+    }
+
+    @Override
+    public int operar() {
+        return operacionCoseno();
+    }
+    
+}
